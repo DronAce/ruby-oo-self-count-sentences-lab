da38a6153596fc794eb3709af6c61d525a0b4482
DronAce <okeynazone@gmail.com>
2020-11-20 04:43:24 -0500

    commit tree ff8b2f46302799ef1ba27fbdd51c5d2822324e2c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
-  def sentence?
     
+  def sentence?
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.count "." 
+    self.question? ? count += 1 : count
   end
 end
